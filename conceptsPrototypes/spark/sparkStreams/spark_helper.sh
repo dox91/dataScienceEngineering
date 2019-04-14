d69ef34fc9c50279eff85e25e3986f14b7429506
anh <anhdo@gmx.net>
2019-04-14 23:15:56 +0200

    spark shell helper

---
+++ conceptsPrototypes/spark/sparkStreams/spark_helper.sh
@@ -0,0 +1,29 @@
+source ../../../initialSetup/set_home_variables.sh > /dev/null 2>&1
+
+
+##############################################
+### submit spark-job: SimpleApp 
+##############################################
+./bin/spark-submit \
+  --class "com.dox91.spark.SimpleApp" \
+  --master local[4] \
+/home/anh/IdeaProjects/SparkApp/target/SparkApp-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+##############################################
+### submit spark-job: KafkaProducerApp
+##############################################
+./bin/spark-submit \
+  --class "com.dox91.spark.KafkaProducerApp" \
+  --master local[4] \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0 \
+/home/anh/IdeaProjects/SparkApp/target/SparkApp-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+##############################################
+### start form spark-shell
+##############################################
+./bin/spark-shell \
+  --class "com.dox91.spark.KafkaProducerApp" \
+  --master local[4] \
+  --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.0 \
+/home/anh/IdeaProjects/SparkApp/target/SparkApp-1.0-SNAPSHOT-jar-with-dependencies.jar
+
