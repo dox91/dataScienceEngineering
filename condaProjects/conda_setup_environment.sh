0784af480fef79cced3b6003f135225eb028d7f2
anh <anhdo@gmx.net>
2019-04-15 23:33:08 +0200

    condaProjects
    setup conda env, yaml

---
+++ condaProjects/conda_setup_environment.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##############################################
+## conda env: bigdata-env
+##############################################
+echo "export PATH=/home/$USER/anaconda3/bin:$PATH" >> ~/.bashrc 
+
+conda env list | grep -q bigData-env
+greprc=$?
+if [[ ! $greprc -eq 0 ]] ; 
+	then
+		echo "create new env"
+		conda env create -f condaEnvYaml/bigData-env.yaml
+fi
+conda env list 
+
+#conda remove --name bigData-env --all
