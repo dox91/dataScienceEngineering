dcda938b5f18c8e89b80728cb5e4992341f5b029
anh <anhdo@gmx.net>
2019-04-19 09:37:37 +0200

    initialSetup
    set_all_home_variables, helper scripts, get data

---
+++ initialSetup/helper/data/get_gdp_data.sh
@@ -0,0 +1,12 @@
+cd $DATA_HOME
+
+wget https://pkgstore.datahub.io/core/gdp/gdp_csv/data/0048bc8f6228d0393d41cac4b663b90f/gdp_csv.csv
+
+cat >gdp_dates_of_interest.csv <<EOL
+1960
+1970 
+1980
+1985
+1995
+1999 
+EOL
---
+++ initialSetup/helper/set_all_home_variables.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+function cdRepoRoot()
+{
+  while [[ $PWD != '/' && ${PWD##*/} != 'dataScienceEngineering' ]]; do cd ..; done
+}
+cdRepoRoot
+export HOME_REPO=$PWD
+echo HOME_REPO=$PWD
+source $HOME_REPO/initialSetup/helper/set_home_variables.sh > /dev/null 2>&1
